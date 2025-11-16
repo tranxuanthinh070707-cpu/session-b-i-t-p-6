03e3b5ab57a2acc1649f502b121d815e36af9e38
tranxuanthinh070707-cpu <tranxuanthinh070707@gmail.com>
2025-11-16 22:03:48 +0700

    Add files via upload

---
+++ bàitap6.sql
@@ -0,0 +1,30 @@
+CREATE TABLE bookes (
+    MaSach INT PRIMARY KEY AUTO_INCREMENT,
+    TenSach VARCHAR(200) NOT NULL,
+    NamXuatBan INT,
+    MaTacGia INT,
+    FOREIGN KEY (MaTacGia) REFERENCES TacGia(MaTacGia)
+);
+
+INSERT INTO bookes (TenSach, NamXuatBan, MaTacGia)
+VALUES
+('Cho tôi xin một vé đi tuổi thơ', 2008, 1),
+('Mắt biếc', 1990, 1),
+('Harry Potter và Hòn đá Phù thủy', 1997, 2),
+('Harry Potter và Phòng chứa Bí mật', 1998, 2),
+('Đắc nhân tâm', 1936, 4);
+
+SELECT *
+FROM bookes
+WHERE NamXuatBan >= 1990
+  AND NamXuatBan <= 2000;
+
+SELECT *
+FROM bookes
+WHERE TenSach = 'Harry Potter và Hòn đá Phù thủy'
+   OR TenSach = 'Harry Potter và Phòng chứa Bí mật';
+
+SELECT *
+FROM bookes
+WHERE MaTacGia = 1
+  AND NamXuatBan > 2000;
